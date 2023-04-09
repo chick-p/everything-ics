cddb9447a528f63095d28085a2b2218270bd82c5
piyo <chick.p.1120@gmail.com>
2023-04-09 18:38:35 +0900

    feat: fetch url

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import { Hono } from "hono";
 const app = new Hono();
 app.get("/", (context) => context.text("Hello 🔥"));
 
-app.get("/ics", (context) => {
+app.get("/ics", async (context) => {
   const url = context.req.query("url");
-  return context.text(`url: ${url}`);
+  if(!url) {
+    return context.notFound();
+  }
+  const response = await fetch(url);
+  if(!response.ok) {
+    return context.notFound();
+  }
+  const body = await response.text();
+  return context.text(body);
 });
 
 export default app;
