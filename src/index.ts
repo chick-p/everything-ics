0cf99b238a58827423d8fde282108da2a60fe4ec
piyo <chick.p.1120@gmail.com>
2023-04-09 18:11:15 +0900

    feat: pass url by query parameter

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ import { Hono } from "hono";
 const app = new Hono();
 app.get("/", (context) => context.text("Hello 🔥"));
 
+app.get("/ics", (context) => {
+  const url = context.req.query("url");
+  return context.text(`url: ${url}`);
+});
+
 export default app;
