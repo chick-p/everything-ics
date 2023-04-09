3d2695365f67e37474f12ceae6f3d237bc556d77
piyo <chick.p.1120@gmail.com>
2023-04-09 18:46:57 +0900

    feat: extract title

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 import { Hono } from "hono";
 
+
+const extractRegex = (str: string, regex: RegExp): string => {
+  const matcher = str.match(regex);
+  if (matcher && matcher.length !== 0) {
+    return matcher[1];
+  }
+  return "";
+};
+
 const app = new Hono();
+
 app.get("/", (context) => context.text("Hello 🔥"));
 
 app.get("/ics", async (context) => {
   const url = context.req.query("url");
-  if(!url) {
+  if (!url) {
     return context.notFound();
   }
   const response = await fetch(url);
-  if(!response.ok) {
-    return context.notFound();
+  if (!response.ok) {
   }
   const body = await response.text();
-  return context.text(body);
+  const title = extractRegex(body, /<title>(.*?)<\/title>/);
+
+  return context.text(title);
 });
 
 export default app;
