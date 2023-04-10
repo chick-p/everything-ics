d02c228da23516faf8137661fdce5d16427f3a9c
piyo <chick.p.1120@gmail.com>
2023-04-10 23:26:31 +0900

    test: add test for full date string

--- src/test/extract.ts
+++ src/test/extract.ts
@@ -20,4 +20,22 @@ describe("extractDate", () => {
     const date = extractDate(body);
     expect(date).toEqual(null);
   });
+
+  it("should return date with full date string", () => {
+    const body = "今日は2022年12月23日です";
+    const date = extractDate(body);
+    expect(date).toEqual(new Date(2022, 11, 23));
+  });
+
+  it("should return first date with multiple date strings", () => {
+    const body = "今日は12月23日で明日は12月24日です";
+    const date = extractDate(body);
+    expect(date).toEqual(new Date(currentYear, 11, 23));
+  });
+
+  it("should return full date with date and full date strings", () => {
+    const body = "今日は12月23日で明日は2022年12月24日です";
+    const date = extractDate(body);
+    expect(date).toEqual(new Date(2022, 11, 24));
+  });
 });
