cd053dfbec0ab6fd2842aed779464efb9c53ec7b
piyo <chick.p.1120@gmail.com>
2023-04-10 21:01:51 +0900

    test: add test with url undefined

--- src/test/index.ts
+++ src/test/index.ts
@@ -8,3 +8,12 @@ describe("GET /", () => {
     expect(body).toContain("everything-ics");
   });
 });
+
+describe("GET /ics", () => {
+  it("should be 404 when url is undefined", async () => {
+    const res = await app.request("http://localhost/ics" );
+    expect(res.status).toBe(404);
+    const body = await res.text();
+    expect(body).toContain("404 Not Found");
+  });
+});
