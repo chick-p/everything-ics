66def319b0207014cd338fe2805f3ae8c92f009c
piyo <chick.p.1120@gmail.com>
2023-04-10 22:44:52 +0900

    refactor: extract extract

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
 import { Hono } from "hono";
 
 import { generateIcs } from "./ics";
-
-const extractRegex = (str: string, regex: RegExp): string => {
-  const matcher = str.match(regex);
-  if (matcher && matcher.length !== 0) {
-    return matcher[1];
-  }
-  return "";
-};
-
-const getEventDate = (dateString: string): Date => {
-  const dateArray = dateString.split(/\s/).join().replace(/日/, "").split(/月/);
-  const [monthString, dayString] = dateArray;
-  const date = new Date();
-  date.setMonth(parseInt(monthString, 10) - 1);
-  date.setDate(parseInt(dayString, 10));
-  date.setHours(0);
-  date.setMinutes(0);
-  date.setSeconds(0);
-  date.setMilliseconds(0);
-  return date;
-};
+import { extractDate, extractRegex } from "./extract";
 
 const app = new Hono();
 
@@ -78,12 +58,11 @@ app.get("/ics", async (context) => {
   const body = await response.text();
 
   const title = extractRegex(body, /<title>(.*?)<\/title>/);
-  // HTML からそれっぽい日付を抽出
-  const dateString = extractRegex(body, /(\d+\s*月\s*\d+\s*日)/);
+  const date = extractDate(body);
 
   const ics = generateIcs({
     title,
-    date: getEventDate(dateString),
+    date,
     url,
   });
   return context.text(ics, 200, {
