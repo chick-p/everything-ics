e841923b2418e200b8e2814c701dcfa578d83df0
piyo <chick.p.1120@gmail.com>
2023-04-10 22:39:21 +0900

    refactor: introduce url variables

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,8 +1,10 @@
 import app from "../index";
 
+const appServer = "http://localhost";
+
 describe("GET /", () => {
   it("should be ok", async () => {
-    const res = await app.request("http://localhost/");
+    const res = await app.request(`${appServer}/`);
     expect(res.status).toBe(200);
     const body = await res.text();
     expect(body).toContain("everything-ics");
@@ -11,7 +13,7 @@ describe("GET /", () => {
 
 describe("GET /ics", () => {
   it("should be 404 when url is undefined", async () => {
-    const res = await app.request("http://localhost/ics" );
+    const res = await app.request(`${appServer}/ics`);
     expect(res.status).toBe(404);
     const body = await res.text();
     expect(body).toContain("404 Not Found");
