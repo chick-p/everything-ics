6e23fa5d9764b62c9dc359e1de587a4450ebf82f
piyo <chick.p.1120@gmail.com>
2023-04-11 12:25:17 +0900

    chore: refactor test description

--- src/test/extract.ts
+++ src/test/extract.ts
@@ -3,13 +3,13 @@ import { extractDate } from "../extract";
 describe("extractDate", () => {
   const currentYear = new Date().getFullYear();
 
-  it("should return date", () => {
+  it("should return short date", () => {
     const body = "今日は4月7日です";
     const date = extractDate(body);
     expect(date).toEqual(new Date(currentYear, 3, 7));
   });
 
-  it("should return date with white spaces", () => {
+  it("should return short date with white spaces", () => {
     const body = "今日は 4 月 7 日です";
     const date = extractDate(body);
     expect(date).toEqual(new Date(currentYear, 3, 7));
