1a10b915f95a5da00254f8d1ed4d3e2654fc36f4
piyo <chick.p.1120@gmail.com>
2023-04-11 21:17:13 +0900

    refactor: gather sources for test

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  testMatch: ["**/test/**/*.ts"],
+  testMatch: ["**/test/**/*.test.ts"],
   transform: {
     "^.+\\.(ts|tsx)$": "esbuild-jest",
   },
   testEnvironment: "miniflare",
   moduleNameMapper: {
-    __STATIC_CONTENT_MANIFEST: "<rootDir>/src/manifest.ts",
+    __STATIC_CONTENT_MANIFEST: "<rootDir>/src/test/manifest.ts",
   },
 };
