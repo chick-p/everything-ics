179a689ce986d07ba13bfa0520a890da60204979
piyo <chick.p.1120@gmail.com>
2023-04-11 12:47:50 +0900

    test: add test for generating ics

--- src/test/index.ts
+++ src/test/index.ts
@@ -20,6 +20,7 @@ describe("GET /ics", () => {
     const origin = fetchMock.get(exampleServer);
     origin.intercept({ method: "GET", path: "/404" }).reply(404, "not found");
     origin.intercept({ method: "GET", path: "/nodate" }).reply(200, "no date");
+    origin.intercept({ method: "GET", path: "/date" }).reply(200, "3月9日");
   });
 
   it("should be 404 when url is undefined", async () => {
@@ -39,5 +40,12 @@ describe("GET /ics", () => {
       `${appServer}/ics?url=${exampleServer}/nodate`
     );
     expect(res.status).toBe(404);
+  });
+
+  it("should be 200 when url has date", async () => {
+    const res = await app.request(`${appServer}/ics?url=${exampleServer}/date`);
+    expect(res.status).toBe(200);
+    const body = await res.text();
+    expect(body).toContain("VCALENDAR");
   });
 });
