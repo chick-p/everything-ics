5cab92b8feed5677b146056622257c5b9e0f4e1a
piyo <chick.p.1120@gmail.com>
2023-04-13 12:49:11 +0900

    feat: sort date candidates

--- src/pages/edit.ts
+++ src/pages/edit.ts
@@ -1,5 +1,6 @@
 import { html } from "hono/html";
 import { Layout } from "../components/Layout";
+import { sortDateByAsc } from "../extract";
 
 type Event = {
   title: string;
@@ -14,40 +15,47 @@ const formattedDate = (date: Date) => {
   return `${year}年${month}月${day}日`;
 };
 
-const dateList = (props: { date: Date }) => {
-  const { date } = props;
-  return html` <option value="${date.toISOString()}">
+const dateList = (props: { date: Date; highPriorityDate: Date }) => {
+  const { date, highPriorityDate } = props;
+  return html` <option
+    value="${date.toISOString()}"
+    ${date === highPriorityDate ? "selected" : ""}
+  >
     ${formattedDate(date)}
   </option>`;
 };
 
-const content = ({ title, candidateDates, url }: Event) => html`
-  <section>
-    <form action="/ics" method="POST">
-      <div>
-        <label for="title">Title</label>
-        <input
-          name="title"
-          id="title"
-          type="text"
-          value="${title}"
-          placeholder="title"
-          class="c-input"
-        />
-      </div>
-      <div>
-        <label for="date">Date</label>
-        <select name="date" id="date" class="c-input">
-          ${candidateDates.map((date) => dateList({ date }))}
-        </select>
-      </div>
-      <input name="url" type="hidden" value="${url}" />
-      <div>
-        <button type="submit" class="c-button">Generate ics</button>
-      </div>
-    </form>
-  </section>
-`;
+const content = ({ title, candidateDates, url }: Event) => {
+  const highPriorityDate = candidateDates[0];
+  const sortDates = sortDateByAsc(candidateDates);
+  return html`
+    <section>
+      <form action="/ics" method="POST">
+        <div>
+          <label for="title">Title</label>
+          <input
+            name="title"
+            id="title"
+            type="text"
+            value="${title}"
+            placeholder="title"
+            class="c-input"
+          />
+        </div>
+        <div>
+          <label for="date">Date</label>
+          <select name="date" id="date" class="c-input">
+            ${sortDates.map((date) => dateList({ date, highPriorityDate }))}
+          </select>
+        </div>
+        <input name="url" type="hidden" value="${url}" />
+        <div>
+          <button type="submit" class="c-button">Generate ics</button>
+        </div>
+      </form>
+    </section>
+  `;
+};
 
 export const Edit = (props: { event: Event; appName: string }) => {
   const children = content(props.event);
