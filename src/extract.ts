944336f18171a45cf859bef33de339c4ec9eb948
piyo <chick.p.1120@gmail.com>
2023-04-24 12:43:07 +0900

    fix: remove unused function

--- src/extract.ts
+++ src/extract.ts
@@ -67,11 +67,6 @@ export const extractEventDates = (body: string) => {
   return Object.values(dateMap);
 };
 
-export const getFirstEventDate = (body: string) => {
-  const eventDates = extractEventDates(body);
-  return eventDates?.[0] || null;
-};
-
 export const sortDateByAsc = (dates: Array<Date>) =>
   dates.sort((a, b) => (a > b ? 1 : -1));
 
