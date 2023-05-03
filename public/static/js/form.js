7b49800e6d7fc17bf4b5e4593420597985b86a90
piyo <chick.p.1120@gmail.com>
2023-05-03 21:48:00 +0900

    refactor: extract function

--- public/static/js/form.js
+++ public/static/js/form.js
@@ -2,28 +2,32 @@
 {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   window.addEventListener("DOMContentLoaded", (_) => {
-    // toggle to display To date
-    const multipleDateElement = document.querySelector("#multiple-dates");
-    const dateToElement = document.querySelector(".c-date-to");
-    const labelForDateFromElement = document.querySelector(
-      ".c-date-from--label"
-    );
-    if (multipleDateElement && dateToElement) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      multipleDateElement.addEventListener("change", (_e) => {
-        const isMultipleDates = multipleDateElement.checked;
-        if (isMultipleDates) {
-          dateToElement.classList.remove("hidden");
-          if (labelForDateFromElement) {
-            labelForDateFromElement.textContent = "From";
+    const toggleMultipleDate = () => {
+      // toggle to display To date
+      const multipleDateElement = document.querySelector("#multiple-dates");
+      const dateToElement = document.querySelector(".c-date-to");
+      const labelForDateFromElement = document.querySelector(
+        ".c-date-from--label"
+      );
+      if (multipleDateElement && dateToElement) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        multipleDateElement.addEventListener("change", (_e) => {
+          const isMultipleDates = multipleDateElement.checked;
+          if (isMultipleDates) {
+            dateToElement.classList.remove("hidden");
+            if (labelForDateFromElement) {
+              labelForDateFromElement.textContent = "From";
+            }
+          } else {
+            dateToElement.classList.add("hidden");
+            if (labelForDateFromElement) {
+              labelForDateFromElement.textContent = "Date";
+            }
           }
-        } else {
-          dateToElement.classList.add("hidden");
-          if (labelForDateFromElement) {
-            labelForDateFromElement.textContent = "Date";
-          }
-        }
-      });
-    }
+        });
+      }
+    };
+
+    toggleMultipleDate();
   });
 }
