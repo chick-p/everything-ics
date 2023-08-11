0b39f467ebd0ec8e4bd8761750b8da2e9ed7d195
mallowlabs <mallowlabs@gmail.com>
2023-08-11 10:12:07 +0900

    Refactor registerToggleMultipleDate

--- public/static/js/form.js
+++ public/static/js/form.js
@@ -7,27 +7,21 @@
     const registerToggleMultipleDate = () => {
       // toggle to display To date
       const multipleDateElement = document.querySelector("#multiple-dates");
-      const dateToElement = document.querySelector(".c-date-to");
-      const labelForDateFromElement = document.querySelector(
-        ".c-date-from--label",
-      );
-      if (multipleDateElement && dateToElement) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        multipleDateElement.addEventListener("change", (_e) => {
-          const isMultipleDates = multipleDateElement.checked;
-          if (isMultipleDates) {
-            dateToElement.classList.remove("hidden");
-            if (labelForDateFromElement) {
-              labelForDateFromElement.textContent = "From";
-            }
-          } else {
-            dateToElement.classList.add("hidden");
-            if (labelForDateFromElement) {
-              labelForDateFromElement.textContent = "Date";
-            }
-          }
-        });
-      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      multipleDateElement.addEventListener("change", (_e) => {
+        const dateToElement = document.querySelector(".c-date-to");
+        const labelForDateFromElement = document.querySelector(
+          ".c-date-from--label",
+        );
+
+        if (multipleDateElement.checked) {
+          dateToElement.classList.remove("hidden");
+          labelForDateFromElement.textContent = "From";
+        } else {
+          dateToElement.classList.add("hidden");
+          labelForDateFromElement.textContent = "Date";
+        }
+      });
     };
 
     // display: none not working for options in Safari
